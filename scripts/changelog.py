aaf65af90b1f29ef218761e67393145cc26bb97d
Mark Skelton <mdskelton99@gmail.com>
2019-04-06 18:09:51 -0500

    Update changelog script

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -2,12 +2,13 @@ import os
 import markdown
 
 SOURCE_FILE = os.path.join(os.path.dirname(__file__), '..', 'CHANGELOG.md')
-DEST_FILE = os.path.join(
-    os.path.dirname(__file__),
-    '..',
-    'build',
-    'CHANGELOG.html'
-)
+DEST_PATH = os.path.join(os.path.dirname(__file__), '..', 'build')
 
 if __name__ == '__main__':
-    markdown.markdownFromFile(input=SOURCE_FILE, output=DEST_FILE)
+    if not os.path.exists(DEST_PATH):
+        os.makedirs(DEST_PATH)
+
+    markdown.markdownFromFile(
+        input=SOURCE_FILE,
+        output=os.path.join(DEST_PATH, 'CHANGELOG.html')
+    )
