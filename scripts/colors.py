cf5e4e7f17feff8c0717e7383b41d40785ddeb80
Mark Skelton <mdskelton99@gmail.com>
2019-04-07 18:07:53 -0500

    Add utility to replace colors with variables

---
+++ scripts/colors.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+
+CONFIG_DIR = os.path.join(os.path.dirname(__file__), 'config')
+
+
+if __name__ == '__main__':
+    with open(os.path.join(CONFIG_DIR, 'colors.yaml'), 'r') as input_file:
+        colors = yaml.load(input_file, Loader=yaml.FullLoader)
+
+    theme_file_path = os.path.join(CONFIG_DIR, 'theme.yaml')
+
+    with open(theme_file_path, 'r') as theme_file:
+        content = theme_file.read()
+
+    for name, value in colors.items():
+        content = content.replace(value, name)
+
+    with open(theme_file_path, 'w') as theme_file:
+        theme_file.write(content)
