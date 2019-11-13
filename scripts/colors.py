5ad730f3e807139cbb11695e34944906dcbbb8b4
Mark Skelton <mdskelton99@gmail.com>
2019-11-13 17:42:30 -0600

    Fix colors script

--- scripts/colors.py
+++ scripts/colors.py
@@ -5,7 +5,7 @@ CONFIG_DIR = os.path.join(os.path.dirname(__file__), 'config')
 
 
 if __name__ == '__main__':
-    with open(os.path.join(CONFIG_DIR, 'colors.yaml'), 'r') as input_file:
+    with open(os.path.join(CONFIG_DIR, 'colors', 'normal.yaml'), 'r') as input_file:
         colors = yaml.load(input_file, Loader=yaml.FullLoader)
 
     theme_file_path = os.path.join(CONFIG_DIR, 'theme.yaml')
