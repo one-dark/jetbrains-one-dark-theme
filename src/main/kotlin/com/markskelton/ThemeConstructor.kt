f23632d415167fc5bbf8faa32a2350e8e5bb9951
DATBOI <alexsimons9999@gmail.com>
2020-05-22 05:43:02 -0500

    Added xml generation functions

--- src/main/kotlin/com/markskelton/ThemeConstructor.kt
+++ src/main/kotlin/com/markskelton/ThemeConstructor.kt
@@ -3,9 +3,18 @@ package com.markskelton
 import com.intellij.ide.ui.laf.LafManagerImpl
 import com.intellij.ide.ui.laf.TempUIThemeBasedLookAndFeelInfo
 import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.vfs.VfsUtil
 import com.markskelton.OneDarkThemeManager.ONE_DARK_ID
 import com.markskelton.settings.ThemeSettings
+import groovy.util.Node
+import groovy.util.XmlNodePrinter
+import java.io.BufferedOutputStream
+import java.io.OutputStreamWriter
+import java.io.PrintWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import javax.swing.UIManager
 
@@ -17,10 +26,65 @@ object ThemeConstructor {
       .first {
         it.theme.id == ONE_DARK_ID
       }
-    val editorPath = Paths.get("/home/alex/workspace/doki-theme-jetbrains/src/main/resources/doki/themes/danganronpa/Mioda_Ibuki_Dark.xml")
     return TempUIThemeBasedLookAndFeelInfo(
       oneDarkLAF.theme,
-      VfsUtil.findFile(editorPath, true)
+      VfsUtil.findFile(getUpdatedEditorScheme(newSettings), true)
     )
+  }
+
+  private fun getUpdatedEditorScheme(themeSettings: ThemeSettings): Path {
+    val newEditorSchemeFile = Paths.get(getAssetsDirectory().toAbsolutePath().toString(), "one_dark.xml")
+    buildNewEditorScheme(themeSettings, newEditorSchemeFile)
+    return newEditorSchemeFile
+  }
+
+  private fun buildNewEditorScheme(themeSettings: ThemeSettings, newSchemeFile: Path) {
+    val colorPalette = getColorPalette(themeSettings)
+    val fontVariants = getFontVariants(themeSettings)
+    val editorTemplate = getEditorXMLTemplate()
+    val updatedScheme = applySettingsToTemplate(
+      editorTemplate,
+      fontVariants,
+      colorPalette
+    )
+    writeXmlToFile(newSchemeFile, updatedScheme)
+  }
+
+  private fun applySettingsToTemplate(
+    editorTemplate: Node,
+    fontVariants: Map<String, String>,
+    colorPalette: Map<String, String>
+  ): Node {
+    TODO("Not yet implemented")
+  }
+
+  private fun getEditorXMLTemplate(): Node {
+    TODO("Not yet implemented")
+  }
+
+  private fun getFontVariants(themeSettings: ThemeSettings): Map<String, String> {
+    TODO("Not yet implemented")
+  }
+
+  private fun getColorPalette(themeSettings: ThemeSettings): Map<String, String> {
+    TODO("Not yet implemented")
+  }
+
+  private fun getAssetsDirectory(): Path {
+    val configDirectory = Paths.get(PathManager.getConfigPath(), "oneDarkAssets")
+    if (Files.notExists(configDirectory)) {
+      Files.createDirectories(configDirectory)
+    }
+    return configDirectory
+  }
+
+  private fun writeXmlToFile(pluginXml: Path, parsedPluginXml: Node) {
+    Files.newOutputStream(pluginXml).use {
+      val outputStream = BufferedOutputStream(it)
+      val writer = PrintWriter(OutputStreamWriter(outputStream, StandardCharsets.UTF_8))
+      val printer = XmlNodePrinter(writer)
+      printer.isPreserveWhitespace = true
+      printer.print(parsedPluginXml)
+    }
   }
 }
