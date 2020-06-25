2be97f9eaae57b18ab5d5d3bb7feda5ff0f1fa1f
Alex Simons <alexsimons9999@gmail.com>
2020-06-25 17:03:42 -0500

    New Build Process (#157)
    * Start of gradle build process
    
    * Need this for initial load, generated colors will come after
    
    * Can patch xml correctly
    
    * Prettier HTML
    
    * Cleaning up the lint
    
    * Updated artifact being copied
    
    * Need to build the changelogs before building the plugin
    
    * The artifact is in the distributions directory..
    
    * Update buildSrc/src/main/kotlin/CreateReleaseNotes.kt
    
    Co-authored-by: Mark Skelton <mdskelton99@gmail.com>
    
    Co-authored-by: Mark Skelton <mdskelton99@gmail.com>

---
+++ buildSrc/src/main/kotlin/one-dark-theme-plugin.gradle.kts
@@ -0,0 +1,3 @@
+tasks.register("patchReadMeHtml", PatchReadmeHTML::class.java)
+tasks.register("createReleaseNotes", CreateReleaseNotes::class.java)
+tasks.register("copyReadme", CopyReadme::class.java)
