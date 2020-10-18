3fa94a95063f4f95ddd2cf07e65e143365dd0a36
Alex Simons <alexsimons9999@gmail.com>
2020-10-18 09:02:35 -0500

    v5.1.2  (#193)
    * Fixed issue with file color shadowing
    
    * Updated changelog and enhanced 2020.3 welcome screen

--- src/main/kotlin/com/markskelton/notification/Notifications.kt
+++ src/main/kotlin/com/markskelton/notification/Notifications.kt
@@ -11,8 +11,9 @@ import com.intellij.ui.IconManager
 val UPDATE_MESSAGE: String = """
       What's New?<br>
       <ul>
-        <li>2020.3 Build Support.</li>
-        <li>Better VCS commit view styling.</li>
+        <li>Fixed bug with file colors.</li>
+        <li>Usability issue with IntelliJ Ultimate UML Diagram.</li>
+        <li>Enhanced 2020.3 welcome screen styling.</li>
       </ul>
       <br>Please see the <a href="https://github.com/one-dark/jetbrains-one-dark-theme/blob/master/CHANGELOG.md">Changelog</a> for more details.
       <br>
