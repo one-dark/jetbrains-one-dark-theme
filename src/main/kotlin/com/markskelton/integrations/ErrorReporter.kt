baf517aea307f8368be3a2e9bb75755a0668c51c
DATBOI <alexsimons9999@gmail.com>
2021-07-12 11:45:59 -0500

    Updated Sentry Client and made config on-demand

--- src/main/kotlin/com/markskelton/integrations/ErrorReporter.kt
+++ src/main/kotlin/com/markskelton/integrations/ErrorReporter.kt
@@ -16,6 +16,7 @@ import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.Consumer
 import com.intellij.util.text.DateFormatUtil
 import com.markskelton.settings.ThemeSettings
+import com.markskelton.tools.runSafely
 import com.markskelton.tools.runSafelyWithResult
 import io.sentry.Sentry
 import io.sentry.SentryEvent
@@ -33,28 +34,6 @@ import java.util.stream.Collectors
 class ErrorReporter : ErrorReportSubmitter() {
   override fun getReportActionText(): String = "Report Anonymously"
 
-  companion object {
-
-    init {
-      ApplicationManager.getApplication().executeOnPooledThread {
-        Sentry.init { options: SentryOptions ->
-          options.dsn =
-            RestClient.performGet(
-              "https://jetbrains.assets.unthrottled.io/one-dark/sentry-dsn.txt"
-            )
-              .map { it.trim() }
-              .orElse("https://cb598170e51a44adbf0079abe2d79624@o403546.ingest.sentry.io/5267019?maxmessagelength=50000")
-        }
-      }
-
-      Sentry.setUser(
-        User().apply {
-          this.id = ThemeSettings.instance.userId
-        }
-      )
-    }
-  }
-
   override fun submit(
     events: Array<out IdeaLoggingEvent>,
     additionalInfo: String?,
@@ -63,21 +42,37 @@ class ErrorReporter : ErrorReportSubmitter() {
   ): Boolean {
     return runSafelyWithResult({
       ApplicationManager.getApplication().executeOnPooledThread {
-        events.forEach {
-          Sentry.captureEvent(
-            addSystemInfo(
-              SentryEvent()
-                .apply {
-                  this.level = SentryLevel.ERROR
-                  this.serverName = getAppName().second
-                  this.setExtra("Additional Info", additionalInfo ?: "None")
-                }
-            ).apply {
-              this.message = Message().apply {
-                this.message = it.throwableText
-              }
+        runSafely({
+          Sentry.setUser(
+            User().apply {
+              this.id = ThemeSettings.instance.userId
             }
           )
+          Sentry.init { options: SentryOptions ->
+            options.dsn =
+              RestClient.performGet(
+                "https://jetbrains.assets.unthrottled.io/one-dark/sentry-dsn-v2.txt"
+              )
+                .map { it.trim() }
+                .orElse("https://0d650a631e3b4170adf6559de446fd37@o403546.ingest.sentry.io/5861304?maxmessagelength=50000")
+          }
+          events.forEach {
+            Sentry.captureEvent(
+              addSystemInfo(
+                SentryEvent()
+                  .apply {
+                    this.level = SentryLevel.ERROR
+                    this.serverName = getAppName().second
+                    this.setExtra("Additional Info", additionalInfo ?: "None")
+                  }
+              ).apply {
+                this.message = Message().apply {
+                  this.message = it.throwableText
+                }
+              }
+            )
+          }
+        }) {
         }
       }
       true
@@ -102,24 +97,24 @@ class ErrorReporter : ErrorReportSubmitter() {
       setExtra("Cores", Runtime.getRuntime().availableProcessors())
       setExtra("Registry", getRegistry())
       setExtra("Non-Bundled Plugins", getNonBundledPlugins())
-      setExtra("Current LAF", LafManager.getInstance().currentLookAndFeel?.name)
+      setExtra("Current LAF", LafManager.getInstance().currentLookAndFeel?.name ?: "")
       setExtra("One Dark Config", ThemeSettings.instance.asJson())
     }
   }
 
-  private fun getJRE(properties: Properties): String? {
+  private fun getJRE(properties: Properties): String {
     val javaVersion = properties.getProperty("java.runtime.version", properties.getProperty("java.version", "unknown"))
     val arch = properties.getProperty("os.arch", "")
     return IdeBundle.message("about.box.jre", javaVersion, arch)
   }
 
-  private fun getVM(properties: Properties): String? {
+  private fun getVM(properties: Properties): String {
     val vmVersion = properties.getProperty("java.vm.name", "unknown")
     val vmVendor = properties.getProperty("java.vendor", "unknown")
     return IdeBundle.message("about.box.vm", vmVersion, vmVendor)
   }
 
-  private fun getNonBundledPlugins(): String? {
+  private fun getNonBundledPlugins(): String {
     return Arrays.stream(PluginManagerCore.getPlugins())
       .filter { p -> !p.isBundled && p.isEnabled }
       .map { p -> p.pluginId.idString }.collect(Collectors.joining(","))
@@ -131,7 +126,7 @@ class ErrorReporter : ErrorReportSubmitter() {
   private fun getGC() = ManagementFactory.getGarbageCollectorMXBeans().stream()
     .map { it.name }.collect(Collectors.joining(","))
 
-  private fun getBuildInfo(appInfo: ApplicationInfoImpl): String? {
+  private fun getBuildInfo(appInfo: ApplicationInfoImpl): String {
     var buildInfo = IdeBundle.message("about.box.build.number", appInfo.build.asString())
     val cal = appInfo.buildDate
     var buildDate = ""
