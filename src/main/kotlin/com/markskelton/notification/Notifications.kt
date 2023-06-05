e3f01c4fd4febac414ad81d73d49e32314e45ddc
Alex Simons <alexsimons9999@gmail.com>
2023-06-04 21:06:25 -0500

    Icon loader API.

--- src/main/kotlin/com/markskelton/notification/Notifications.kt
+++ src/main/kotlin/com/markskelton/notification/Notifications.kt
@@ -5,7 +5,7 @@ import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.ui.IconManager
+import com.intellij.openapi.util.IconLoader
 import com.markskelton.OneDarkThemeManager.PLUGIN_ID
 import org.intellij.lang.annotations.Language
 
@@ -15,14 +15,14 @@ val UPDATE_MESSAGE: String = """
       <ul>
           <li>2023.2 Build Support</li>
       </ul>
-      <br>Please see the <a href="https://github.com/one-dark/jetbrains-one-dark-theme/blob/master/CHANGELOG.md">Changelog</a> for more details.
+      <br>Please see the <a href='https://github.com/one-dark/jetbrains-one-dark-theme/blob/master/CHANGELOG.md'>Changelog</a> for more details.
       <br>
       Thank you for choosing the One Dark Theme!<br>
 """.trimIndent()
 
 object Notifications {
 
-  private val NOTIFICATION_ICON = IconManager.getInstance().getIcon(
+  private val NOTIFICATION_ICON = IconLoader.getIcon(
     "/icons/one-dark-logo.svg",
     Notifications::class.java
   )
